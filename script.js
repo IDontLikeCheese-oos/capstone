044652b9fffdce20f19d1860a9eb301af10ffade
IDontLikeCheese-oos <j.miranda@mcaabhm.org>
2026-05-11 22:25:51 -0500

    partially added deck building?
    Will finish this part at school.

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let deck = [];
 let playerHand = [];
 let cpuHand = [];
+
+function createDeck() {
+  deck = [];
+
+  for (let color of colors) {
+    for (let value of values)

6af97c70a2bca55e9541e2dd45e8a374101ce708
IDontLikeCheese-oos <j.miranda@mcaabhm.org>
2026-05-13 14:46:43 -0500

    Reworked create deck and started shuffle deck.

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 const colors = ["Red", "Blue", "Green", "Yellow"];
 const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-let deck = []; // Forms the main deck of card\\
-let playerHand = []; //cards in player hand\\
+let deck = []; // Forms the main deck of card
+let playerHand = []; //cards in player hand
 let cpuHand = [];
+
+let currentCard = null;
+let isPlayerTurn = true;
+let gameOver = false;
 
 function createDeck() {
   deck = [];
 
   for (let color of colors) {
     for (let value of values) {
-   deck.push(color + " " + value); //pushes color and values on cards\\
+   deck.push({ color, value }); //pushes color and values on cards\\
   }
 }
 
-return deck;
-}
-
-  for (let i = 0 < 4; 1++) {
+for (let i = 0 < 4; 1++) {
     deck.push({ color: "wild", value: "wild"});
     deck.push({ color: "wild", value: "wild+4"});
   }
+}
+
+//shuffle cards
+function shuffleDeck {}
